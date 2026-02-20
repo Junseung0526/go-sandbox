b37c4fb7d8c7088fc4539ae9de2e1d84862b4b48
Junseung <rla005@naver.com>
2026-02-20 19:47:36 +0900

    fix: CORS

--- middleware/cors.go
+++ middleware/cors.go
@@ -4,14 +4,17 @@ import "github.com/gin-gonic/gin"
 
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*") // 모든 도메인 허용
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 
+		// Preflight 요청 처리
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
 			return
 		}
+
 		c.Next()
 	}
 }
